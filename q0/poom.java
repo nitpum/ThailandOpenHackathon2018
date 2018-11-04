81713ee10a67376a21e0acaabfeb1b6a56108a78
nitpum <nitpum@outlook.com>
2018-11-04 23:04:34 +0700

    Remove java from q0

--- q0/poom.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Main {
-  public static void main(String[] args) {
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-    String line = in.readLine();
-    System.out.println(line);
-  }
-}
